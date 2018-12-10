ce41b510656c60dedb1c4a1088f5d7cc79cb6993
CodyD <codydebruin@Msn.com>
2018-12-09 22:16:09 -0700

    arrow chart fixes

--- src/components/stockcard/vertGraph/vertgraph.js
+++ src/components/stockcard/vertGraph/vertgraph.js
@@ -10,38 +10,20 @@ export class VertGraph extends Component {
         }
     }
 
-    
-    
-
     getArrowOffset = () =>{
         const props = this.props.quoteInfo
-        return (((props.high - props.latestPrice) / (props.high - props.low)) * 100).toFixed(2) 
-
-        //.changePercent also provides change
+        console.log((((props.high - props.latestPrice) / (props.high - props.low)) * 100).toFixed(2)    )
+        return (((props.high - props.latestPrice) / (props.high - props.low)) * 100).toFixed(2)     
     }
 
 
     render() {
-         const props = this.props.quoteInfo
-         const color = props.change < 0 ? 'red' : 'green'
-        const divStyle = {
-            transform: `translate(0px, ${this.getArrowOffset()}%)`, 
-            // position: 'realative',
-            // top: this.getArrowOffset()
-            borderRight:`10px solid ${color}`, 
-        };
-
-        
-
+        let offset = this.getArrowOffset()
+         const color = offset > 50 ? 'red' : 'green'
         return (
         <Fragment>
-            
-            <div className={"vertGraph " + this.props.className } > 
-                {/* <div className="farRight">{props.high}</div>  */}
-                <div className="vertContainer"><div className="arrow-left" style={divStyle}> <p>{`$${props.latestPrice}`}</p> </div></div>
-                {/* <p>{props.low}</p> */}
-            </div>
-            
+            <input type="range" min="1" max="100" value={offset} className={color}/>
+            <p>{`$${this.props.quoteInfo.latestPrice}`}</p>
         </Fragment>
         );
     }
